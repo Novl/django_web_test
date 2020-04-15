27d66278885f2b86d208ac75279b1a96778ac8df
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-15 21:33:13 +0300

    edited init for gunicorn

--- 2.1-11/init.sh
+++ 2.1-11/init.sh
@@ -9,3 +9,4 @@ sudo ln -sf /home/box/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/nginx restart
+cd ./ask/ && gunicorn myapp & && cd --
