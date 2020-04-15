225331611ff28d796e4edaf6045d7d43faada82f
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-15 18:39:20 +0300

    fix 6: header

--- 1.9-11/hello.py
+++ 1.9-11/hello.py
@@ -6,7 +6,7 @@ def app(environ, start_response):
     # data = pprint.pformat(environ, indent=4)
     status = '200 OK'
     response_headers = [
-        ('Content-type', 'text/plain'),
+        ('Content-Type', 'text/plain'),
         # ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
