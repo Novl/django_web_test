ba55f349d4fdd83afb201e6d579119818bea6e85
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-15 21:51:06 +0300

    fix 2: nginx proxy config and gunicorn start

--- 2.1-11/init.sh
+++ 2.1-11/init.sh
@@ -8,4 +8,4 @@ sudo ln -sf nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/nginx restart
-cd ./ask/ && (gunicorn myapp &) && cd --
+cd ./ask/ && (gunicorn ask.wsgi:application &) && cd --
