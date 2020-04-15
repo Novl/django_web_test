1cb6dc11f2a310bc1ccb9e9f8bfa4b51ef5a0d4d
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-15 21:39:49 +0300

    fix 1: edited init

--- 2.1-11/init.sh
+++ 2.1-11/init.sh
@@ -3,10 +3,9 @@ echo $cur_dir
 mkdir -p /home/box/web/{etc,public,uploads}
 
 cp -r ./ask/ /home/box/web/
-cp -f nginx.conf /home/box/etc/
 
-sudo ln -sf /home/box/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/nginx restart
-cd ./ask/ && gunicorn myapp & && cd --
+cd ./ask/ && (gunicorn myapp &) && cd --
