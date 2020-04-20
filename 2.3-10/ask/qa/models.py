3f3a2e84c2d960b9c98423a29fdc3f5bd3cab175
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 20:17:22 +0300

    fix 3: foreign key author - user

--- 2.3-10/ask/qa/models.py
+++ 2.3-10/ask/qa/models.py
@@ -15,14 +15,14 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     rating  = models.IntegerField()
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey('User')
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     question = models.TextField()
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey('User')
 
 
 
