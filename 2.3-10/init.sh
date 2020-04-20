f48dfa36ae086977dd470bd6d598d79b33f028e5
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 21:35:24 +0300

    fix 9: added mkdir

--- 2.3-10/init.sh
+++ 2.3-10/init.sh
@@ -2,6 +2,8 @@ cur_dir=$(pwd)
 sudo pip3 install --upgrade django==2
 sudo /etc/init.d/mysql start
 
+mkdir -p /home/box/web/
+
 mysql -u root -e "create database if not exists t23"
 cp -r ./ask/ /home/box/web/ask
 python3 ~/web/ask/manage.py makemigrations
