ad28af2a84d53f05d2329d7114e3d34c4d63aae2
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 19:45:55 +0300

    proper if exists; proper copy

--- 2.3-10/init.sh
+++ 2.3-10/init.sh
@@ -2,5 +2,5 @@ cur_dir=$(pwd)
 
 sudo /etc/init.d/mysql start
 
-mysql -u root -e "create database t23"
-cp -r ./ask/ /home/box/web/
+mysql -u root -e "create database if not exists t23"
+cp -r ./ask/ /home/box/
