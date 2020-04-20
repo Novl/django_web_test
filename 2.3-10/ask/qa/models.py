f89390196658ba69291b9bb23a85b7e005b54486
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 20:07:14 +0300

    edited models: fields and relations

--- 2.3-10/ask/qa/models.py
+++ 2.3-10/ask/qa/models.py
@@ -1,25 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
+    objects = QuestionManager() 
     title = models.CharField()
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     rating  = models.IntegerField()
     author = models.CharField()
-    likes = models.ArrayField(models.CharField())
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.TextField()
     author = models.CharField()
 
 
 class QuestionManager(models.Manager):
     def new(self):
-        pass
+        return self.order_by('-added_at')
 
     def popular(self):
-        pass
+        return self.order_by('-rating')
