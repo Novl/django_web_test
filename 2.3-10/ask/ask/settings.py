6b680cd096853b977dddcd9c4d3e8a68339230b2
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 19:42:35 +0300

    added db

--- 2.3-10/ask/ask/settings.py
+++ 2.3-10/ask/ask/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'NAME': 't23',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
