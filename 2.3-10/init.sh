673916cafcaa77b45d1d73befe01c4a6867a0c8c
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 20:10:01 +0300

    proper path

--- 2.3-10/init.sh
+++ 2.3-10/init.sh
@@ -3,6 +3,6 @@ sudo pip3 install --upgrade django==2
 sudo /etc/init.d/mysql start
 
 mysql -u root -e "create database if not exists t23"
-cp -r ./ask/ /home/box/
+cp -r ./ask/ /home/box/web/
 python3 ~/web/ask/manage.py makemigrations
 python3 ~/web/ask/manage.py migrate
