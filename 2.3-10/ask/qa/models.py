4d6835104a341571d60728e30859436b51cba2bc
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 20:52:12 +0300

    fix 8: edited default

--- 2.3-10/ask/qa/models.py
+++ 2.3-10/ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-    rating  = models.IntegerField()
+    rating  = models.IntegerField(default=0)
     author = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
