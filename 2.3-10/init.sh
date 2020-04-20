d752198ffe2075e90252bf83d8fa9f270c8a5565
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 20:06:37 +0300

    added migrations

--- 2.3-10/init.sh
+++ 2.3-10/init.sh
@@ -1,6 +1,8 @@
 cur_dir=$(pwd)
-
+sudo pip3 install --upgrade django==2
 sudo /etc/init.d/mysql start
 
 mysql -u root -e "create database if not exists t23"
 cp -r ./ask/ /home/box/
+python3 ~/web/ask/manage.py makemigrations
+python3 ~/web/ask/manage.py migrate
