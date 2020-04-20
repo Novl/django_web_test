f0b066ddd74c39229f5373c27d9ac05b42ac2074
Dmitry Chernovol <Novelz@yandex.ru>
2020-04-20 21:40:55 +0300

    fix 11: forced cp

--- 2.3-10/init.sh
+++ 2.3-10/init.sh
@@ -5,6 +5,6 @@ sudo /etc/init.d/mysql start
 mkdir -p /home/box/web/
 
 mysql -u root -e "create database if not exists t23"
-cp -r ./ask/ /home/box/web/ask
+cp -rf ./ask/ /home/box/web/ask
 python3 ~/web/ask/manage.py makemigrations
 python3 ~/web/ask/manage.py migrate
